b09a2f01ab1c79694901e0d2003579292c53356c
Ricky Nguyen <ricky.nguyen981@gmail.com>
2024-03-03 15:11:11 -0500

    Added new delete ability for feedbacks

--- PlatePlan/src/componentPanels/BusinessFeedbackComponent.java
+++ PlatePlan/src/componentPanels/BusinessFeedbackComponent.java
@@ -12,16 +12,12 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-import businessPanels.BusinessFeedbackManagement;
 import businessPanels.BusinessMenuMangement;
-import database.DataBaseFactory;
 import dto.Business;
 import dto.Feedback;
 import dto.MenuItem;
 import main.PlatePlanMain;
-import service_interfaces.FeedbackService;
 import service_interfaces.MenuService;
-import services.FeedbackServiceImpl;
 import services.MenuServiceImpl;
 
 import java.awt.event.KeyAdapter;
@@ -35,15 +31,14 @@ import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.border.BevelBorder;
 
-public class StaticFeedbackComponent extends JPanel {
+public class BusinessFeedbackComponent extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private FeedbackService feedbackService;
 
 	/**
 	 * Create the panel.
 	 */
-	public StaticFeedbackComponent(Feedback feedback) {
+	public BusinessFeedbackComponent(Feedback feedback) {
 		Dimension dimension = new Dimension(400, 100);
 		this.setPreferredSize(new Dimension(540, 150));
 		this.setMinimumSize(new Dimension(600, 150));
@@ -51,13 +46,12 @@ public class StaticFeedbackComponent extends JPanel {
 		setBackground(new Color(248, 248, 255));
 		setLayout(null);
 
-		feedbackService = FeedbackServiceImpl.getInstance();
 		// JTextArea for description
 		JTextArea txtDescription = new JTextArea("FEEDBACK");
 
 		txtDescription.setEditable(false); // This line makes the JTextArea editable
 		txtDescription.setFont(new Font("Arial", Font.PLAIN, 12));
-		txtDescription.setBounds(10, 58, 425, 81);
+		txtDescription.setBounds(10, 58, 500, 81);
 		txtDescription.setWrapStyleWord(true);
 		txtDescription.setLineWrap(true);
 		txtDescription.setOpaque(true);
@@ -65,54 +59,36 @@ public class StaticFeedbackComponent extends JPanel {
 		txtDescription.setBorder(null); // No border to mimic a JLabel
 
 		add(txtDescription);
-
+		
 		JLabel lblTimestamp = new JLabel("TIMESTAMP");
 		lblTimestamp.setFont(new Font("Arial", Font.ITALIC, 14));
 		lblTimestamp.setBounds(214, 24, 143, 24);
 		add(lblTimestamp);
-
+		
 		JLabel lblRating = new JLabel("RATING");
 		lblRating.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 14));
 		lblRating.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRating.setBounds(440, 24, 70, 24);
 		add(lblRating);
-
+		
 		JLabel lblLeftby = new JLabel("LEFT BY");
 		lblLeftby.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblLeftby.setBounds(103, 23, 122, 24);
 		add(lblLeftby);
-
+		
 		JLabel lblPostedBy = new JLabel("Posted By: ");
 		lblPostedBy.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblPostedBy.setBounds(10, 23, 84, 24);
 		add(lblPostedBy);
-
+		
 		JLabel lblRating_2 = new JLabel("Rating: ");
 		lblRating_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRating_2.setFont(new Font("Arial", Font.BOLD, 14));
 		lblRating_2.setBounds(357, 23, 84, 24);
 		add(lblRating_2);
-
-		JButton btnDeleteFeedback = new JButton("Delete");
-		btnDeleteFeedback.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				if (feedbackService.deleteFeedback(feedback)) {
-					JOptionPane.showMessageDialog(null, "Feedback deleted successfully", "Success",
-							JOptionPane.INFORMATION_MESSAGE);
-					PlatePlanMain.switchPanels(
-							new BusinessFeedbackManagement(DataBaseFactory.getDatabase().getBusinessAccount()));
-				} else {
-					JOptionPane.showMessageDialog(null, "Failed to delete feedback", "Failure",
-							JOptionPane.ERROR_MESSAGE);
-
-				}
-			}
-		});
-		btnDeleteFeedback.setBounds(441, 87, 89, 23);
-		add(btnDeleteFeedback);
-
-		if (feedback != null) {
+		
+		if (feedback != null)
+		{
 			lblTimestamp.setText(feedback.getTimestamp().toString());
 			lblRating.setText(feedback.getRating() + "/5");
 			lblLeftby.setText(feedback.getCustomer_id());
@@ -121,4 +97,5 @@ public class StaticFeedbackComponent extends JPanel {
 		}
 
 	}
+
 }

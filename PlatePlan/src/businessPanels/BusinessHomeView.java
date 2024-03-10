364a1d590f7394f0ce34abc8ceaedf78fc1dab8f
farahmadkour816 <FarahMadkour816@gmail.com>
2024-03-10 00:41:32 -0500

    Added icons to business homeview

--- PlatePlan/src/businessPanels/BusinessHomeView.java
+++ PlatePlan/src/businessPanels/BusinessHomeView.java
@@ -4,12 +4,18 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import customerPanels.Constants;
 import customerPanels.CustomerSignIn;
@@ -21,11 +27,11 @@ public class BusinessHomeView extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JLabel lblNewLabel;
 	private Business business;
-	private JButton btnNewButton;
+	private JButton btnReservations;
 	private JButton btnManageTables;
 	private JButton btnManageServers;
-	private JButton btnViewManage;
-	private JButton btnViewManage_2;
+	private JButton btnManageMenu;
+	private JButton btnManageFeedback;
 
 	/**
 	 * Create the panel.
@@ -38,25 +44,116 @@ public class BusinessHomeView extends JPanel {
 		this.setMinimumSize(windowDim);
 		this.setMaximumSize(windowDim);
 		setLayout(null);
-		setBackground(new Color(255, 250, 250));
+		setBackground(Color.decode("#E7F6F2"));
 		setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 		// ===========================================================================
+		
 		this.business = bussiness;
 		lblNewLabel = new JLabel("BUSINESS MANAGEMENT");
-		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 22));
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setFont(new Font("Calibri", Font.ITALIC, 30));
 		lblNewLabel.setLabelFor(this);
-		lblNewLabel.setBounds(429, 105, 370, 28);
+		lblNewLabel.setBounds(375, 122, 349, 38);
 		add(lblNewLabel);
 
+		btnReservations = new JButton("Reservations");
+		btnManageTables = new JButton("Tables");
+		btnManageServers = new JButton("Servers");
+		btnManageMenu = new JButton("Menu");
+		btnManageFeedback = new JButton("Feedbacks");
+
+		Color backgroundColor = Color.decode("#FAF0E6");
+
+		btnReservations.setBackground(backgroundColor);
+		btnManageTables.setBackground(backgroundColor);
+		btnManageServers.setBackground(backgroundColor);
+		btnManageMenu.setBackground(backgroundColor);
+		btnManageFeedback.setBackground(backgroundColor);
+
+		// Add a mouse listener to the button to change the icon on hover
+		btnManageTables.addMouseListener(new MouseAdapter() {
+			Icon hoverIcon = new ImageIcon(new ImageIcon(BusinessHomeView.class.getResource("/tableIcon.png"))
+					.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnManageTables.setIcon(hoverIcon);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnManageTables.setIcon(null); // Remove the icon when the mouse exits the button
+			}
+		});
+
+		btnReservations.addMouseListener(new MouseAdapter() {
+			Icon hoverIcon = new ImageIcon(new ImageIcon(BusinessHomeView.class.getResource("/reservationsIcon.png"))
+					.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnReservations.setIcon(hoverIcon);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnReservations.setIcon(null); // Remove the icon when the mouse exits the button
+			}
+		});
+
+		btnManageServers.addMouseListener(new MouseAdapter() {
+			Icon hoverIcon = new ImageIcon(new ImageIcon(BusinessHomeView.class.getResource("/serverIcon.png"))
+					.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnManageServers.setIcon(hoverIcon);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnManageServers.setIcon(null); // Remove the icon when the mouse exits the button
+			}
+		});
+
+		btnManageMenu.addMouseListener(new MouseAdapter() {
+			Icon hoverIcon = new ImageIcon(new ImageIcon(BusinessHomeView.class.getResource("/menuIcon.png")).getImage()
+					.getScaledInstance(30, 30, Image.SCALE_SMOOTH));
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnManageMenu.setIcon(hoverIcon);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnManageMenu.setIcon(null); // Remove the icon when the mouse exits the button
+			}
+		});
+
+		btnManageFeedback.addMouseListener(new MouseAdapter() {
+			Icon hoverIcon = new ImageIcon(new ImageIcon(BusinessHomeView.class.getResource("/feedbackIcon.png"))
+					.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnManageFeedback.setIcon(hoverIcon);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnManageFeedback.setIcon(null); // Remove the icon when the mouse exits the button
+			}
+		});
+
 		// Button to open another panel(reservationHomeView)
-		btnNewButton = new JButton("View & Manage Reservations");
-		btnNewButton.addActionListener(new ActionListener() {
+		btnReservations.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				PlatePlanMain.switchPanels(new BusinessReservations(business));
 			}
 		});
-		btnNewButton.setBounds(429, 144, 231, 23);
-		add(btnNewButton);
+		btnReservations.setBounds(245, 206, 200, 50);
+		add(btnReservations);
 
 		JButton btnLogOut = new JButton("Log Out");
 		btnLogOut.addActionListener(new ActionListener() {
@@ -64,44 +161,40 @@ public class BusinessHomeView extends JPanel {
 				PlatePlanMain.switchPanels(new CustomerSignIn());
 			}
 		});
-		btnLogOut.setBounds(6, 6, 117, 29);
+		btnLogOut.setBounds(6, 6, 97, 28);
 		add(btnLogOut);
 
-		btnManageTables = new JButton("View & Manage Tables");
 		btnManageTables.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				PlatePlanMain.switchPanels(new BusinessTableManageView(bussiness));
 			}
 		});
-		btnManageTables.setBounds(429, 184, 231, 23);
+		btnManageTables.setBounds(455, 206, 200, 50);
 		add(btnManageTables);
 
-		btnManageServers = new JButton("View & Manage Servers");
 		btnManageServers.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				PlatePlanMain.switchPanels(new BusinessServerManage(bussiness));
 
 			}
 		});
-		btnManageServers.setBounds(429, 223, 231, 23);
+		btnManageServers.setBounds(665, 206, 200, 50);
 		add(btnManageServers);
 
-		btnViewManage = new JButton("View & Manage Menu");
-		btnViewManage.addActionListener(new ActionListener() {
+		btnManageMenu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				PlatePlanMain.switchPanels(new BusinessMenuMangement(bussiness));
 			}
 		});
-		btnViewManage.setBounds(429, 263, 231, 23);
-		add(btnViewManage);
-		
-		btnViewManage_2 = new JButton("View & Manage Feedbacks");
-		btnViewManage_2.addActionListener(new ActionListener() {
+		btnManageMenu.setBounds(245, 317, 200, 50);
+		add(btnManageMenu);
+
+		btnManageFeedback.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				PlatePlanMain.switchPanels(new BusinessFeedbackManagement(bussiness));
 			}
 		});
-		btnViewManage_2.setBounds(429, 297, 231, 23);
-		add(btnViewManage_2);
+		btnManageFeedback.setBounds(455, 317, 200, 50);
+		add(btnManageFeedback);
 	}
 }
